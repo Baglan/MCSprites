a586af72b07a572ce9f49afaa5ec6b5acca37862
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-12-20 13:26:35 +0600

    Initial Commit

---
+++ MCSprites/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCSprites
+//
+//  Created by Baglan on 12/20/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
